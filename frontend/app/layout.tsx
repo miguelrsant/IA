4735b7d8842cbcba95eb8e21cd296178880735e9
miguelrsant <miguelrsant01@gmail.com>
2025-11-22 09:29:36 -0300

    add homepage for the website

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import '../public/styles/index.css'
+
+export const metadata: Metadata = {
+  title: "Taylor: Business Super Intelligence Platform",
+  description: "TAYLOR é uma plataforma avançada de automação operacional e análise inteligente, transformando dados em decisões estratégicas para empresas de todos os setores.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br"
+    data-lt-installed="true">
+      <head>
+        <link rel="shortcut icon" href="./bussine-engrenagem.png"/>
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,32 @@
 export default function Home() {
   return (
-    <h1>Hello</h1>
+    <div className="home">
+      <div className="home-page">
+        <div className="business">
+          <img src="/bussine-engrenagem.png" alt="Engrenagem" />
+          <p>Business Super Intelligence Platform</p>
+        </div>
+        <div className="texto-home">
+          <h1 className="taylor-principal">TAYLOR</h1>
+          <h1 className="taylor-secundario">Transforme dados em decisões estratégicas</h1>
+          <p className="taylor-paragrafo">Plataforma avançada de automação operacional e análise inteligente com IA. Automatize rotinas, gere insights e acompanhe seu negócio em tempo real.</p>
+        </div>
+        <div className="btn-home"><p>Começar agora →</p></div>
+      </div>
+      <div className="vantagens">
+        <div className="cards-home">
+          <h1 className="cards-h1">99%</h1>
+          <p className="cards-p">Redução do tempo</p>
+        </div>
+        <div className="cards-home">
+          <h1 className="cards-h1">24/7</h1>
+          <p className="cards-p">Análise automática</p>
+        </div>
+        <div className="cards-home">
+          <h1 className="cards-h1">100%</h1>
+          <p className="cards-p">Personalizável</p>
+        </div>
+      </div>
+    </div>
   );
 }
